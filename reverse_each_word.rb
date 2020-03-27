86d1431118d363cf3b2b55fb9ad7ded9ac7387cc
David Hutchison <dhutchis68516@gmail.com>
2020-03-27 19:41:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
-  sentence.split(" ")
-  sentence.collect do |array| 
-    sentence.reverse
+  array = sentence.split(" ")
+  array.collect do |array| 
+    array.reverse
     end
     
  end
